3a895f0ae049e767c667c779498bba1867ec91dd
Hitoshi Amano <seijro@gmail.com>
2024-05-13 05:46:31 +0900

    run graph asynchronous

--- src/face_mesh_async.cc
+++ src/face_mesh_async.cc
@@ -1,3 +1,4 @@
+#include <csignal>
 #include <cstdlib>
 
 #include "absl/flags/flag.h"
@@ -14,16 +15,31 @@
 #include "mediapipe/framework/port/status.h"
 #include "mediapipe/util/resource_util.h"
 
+volatile std::sig_atomic_t gSignalStatus;
+
 static_assert((CV_MAJOR_VERSION >= 3) && (CV_MINOR_VERSION >= 2), "Only opencv 3.2+ is supported.");
 
 constexpr char kWindowName[] = "MediaPipe";
 
-ABSL_FLAG(std::string, input_video_path, "", "Full path of video to load. If not provided, attempt to use a webcam.");
+ABSL_FLAG(std::string, input_video_path, "", "path of video to load");
+ABSL_FLAG(std::string, output_video_path, "", "path of video to save (mp4 only)");
+
+void signal_handler(int signal) {
+    gSignalStatus = signal;
+}
 
 absl::Status RunMPPGraph() {
     LOG(INFO) << "Start running MediaPipe graph.";
+
+    std::signal(SIGINT, signal_handler);
+
     std::string video_path = absl::GetFlag(FLAGS_input_video_path);
     RET_CHECK(!video_path.empty());
+
+    std::string output_path = absl::GetFlag(FLAGS_output_video_path);
+    if (output_path.empty()) {
+        output_path = "output.mp4";
+    }
 
     mediapipe::CalculatorGraphConfig config = mediapipe::ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig>(R"pb(
         input_stream: "in"
@@ -68,15 +84,35 @@ absl::Status RunMPPGraph() {
     mediapipe::CalculatorGraph graph;
     MP_RETURN_IF_ERROR(graph.Initialize(config));
 
+    std::shared_ptr<std::mutex> video_writer_mutex = std::make_shared<std::mutex>();
+    std::shared_ptr<cv::VideoWriter> video_writer = std::make_shared<cv::VideoWriter>();
+    RET_CHECK(!video_writer->isOpened());
+
+    LOG(INFO) << "Initialize observer callback.";
+    auto cb = [video_writer, video_writer_mutex](const mediapipe::Packet &packet) -> absl::Status {
+        const mediapipe::ImageFrame& output_frame = packet.Get<mediapipe::ImageFrame>();
+
+
+        // Convert back to opencv for display or saving.
+        cv::Mat output_frame_mat = mediapipe::formats::MatView(&output_frame);
+        assert(output_frame_mat.data != nullptr);
+        cv::cvtColor(output_frame_mat, output_frame_mat, cv::COLOR_RGB2BGR);
+
+        {
+            assert(video_writer->isOpened());
+            std::lock_guard<std::mutex> lock(*video_writer_mutex);
+
+            video_writer->write(output_frame_mat);
+        }
+
+        return absl::OkStatus();
+    };
+    MP_RETURN_IF_ERROR(graph.ObserveOutputStream("out", cb));
+
     LOG(INFO) << "Making video stream from: " << video_path;
     cv::VideoCapture capture;
     capture.open(std::move(video_path));
     RET_CHECK(capture.isOpened());
-    capture.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-    capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-    capture.set(cv::CAP_PROP_FPS, 30);
-
-    cv::namedWindow(kWindowName, /*flags=WINDOW_AUTOSIZE*/ 1);
 
     LOG(INFO) << "Start running the MediaPipe graph.";
     MP_ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller, graph.AddOutputStreamPoller("out"));
@@ -85,48 +121,48 @@ absl::Status RunMPPGraph() {
     LOG(INFO) << "Start grabbing and processing frames.";
     while (true) {
         // Capture opencv camera or video frame.
-        cv::Mat camera_frame_raw;
-        capture >> camera_frame_raw;
-        if (camera_frame_raw.empty()) {
+        cv::Mat input_frame_bgr_mat;
+        capture >> input_frame_bgr_mat;
+        if (input_frame_bgr_mat.empty()) {
             LOG(INFO) << "Empty frame, end of video reached.";
             break;
         }
-        cv::Mat camera_frame;
-        cv::cvtColor(camera_frame_raw, camera_frame, cv::COLOR_BGR2RGB);
+        cv::Mat input_frame_rgb_mat;
+        cv::cvtColor(input_frame_bgr_mat, input_frame_rgb_mat, cv::COLOR_BGR2RGB);
+
+        {
+            std::lock_guard<std::mutex> lock(*video_writer_mutex);
+            if (!video_writer->isOpened()) {
+                video_writer->open(output_path, mediapipe::fourcc('a', 'v', 'c', '1'), 30, cv::Size(input_frame_rgb_mat.cols, input_frame_rgb_mat.rows));
+            }
+        }
 
         // Wrap Mat into an ImageFrame.
-        auto input_frame = absl::make_unique<mediapipe::ImageFrame>(mediapipe::ImageFormat::SRGB, camera_frame.cols, camera_frame.rows, mediapipe::ImageFrame::kDefaultAlignmentBoundary);
+        auto input_frame = absl::make_unique<mediapipe::ImageFrame>(mediapipe::ImageFormat::SRGB, input_frame_rgb_mat.cols, input_frame_rgb_mat.rows, mediapipe::ImageFrame::kDefaultAlignmentBoundary);
         cv::Mat input_frame_mat = mediapipe::formats::MatView(input_frame.get());
-        camera_frame.copyTo(input_frame_mat);
+        input_frame_rgb_mat.copyTo(input_frame_mat);
 
         // Send image packet into the graph.
         size_t frame_timestamp_us = (double)cv::getTickCount() / (double)cv::getTickFrequency() * 1e6;
         MP_RETURN_IF_ERROR(graph.AddPacketToInputStream("in", mediapipe::Adopt(input_frame.release()).At(mediapipe::Timestamp(frame_timestamp_us))));
 
-        // Get the graph result packet, or stop if that fails.
-        mediapipe::Packet packet;
-        if (!poller.Next(&packet)) {
-            break;
-        }
-        const mediapipe::ImageFrame& output_frame = packet.Get<mediapipe::ImageFrame>();
-
-        // Convert back to opencv for display or saving.
-        cv::Mat output_frame_mat = mediapipe::formats::MatView(&output_frame);
-        cv::cvtColor(output_frame_mat, output_frame_mat, cv::COLOR_RGB2BGR);
-
-        // Display the output frame.
-        cv::imshow(kWindowName, output_frame_mat);
-
-        // Press any key to exit.
-        const int pressed_key = cv::waitKey(5);
-        if (pressed_key >= 0 && pressed_key != 255) {
+        if (gSignalStatus) {
+            LOG(INFO) << "Interrupt signal received.";
             break;
         }
     }
 
     MP_RETURN_IF_ERROR(graph.CloseInputStream("in"));
+    MP_RETURN_IF_ERROR(graph.WaitUntilDone());
 
-    return graph.WaitUntilDone();
+    {
+        std::lock_guard<std::mutex> lock(*video_writer_mutex);
+        if (video_writer->isOpened()) {
+            video_writer->release();
+        }
+    }
+
+    return absl::OkStatus();
 }
 
 int main(int argc, char** argv) {
@@ -141,4 +177,5 @@ int main(int argc, char** argv) {
     }
     return EXIT_SUCCESS;
 }
+
 
