61d0c6a1661bbfbaf8148f210fa83af338efef81
Hitoshi Amano <seijro@gmail.com>
2024-05-15 11:40:36 +0900

    smoothing rect

--- src/crop_human.cc
+++ src/crop_human.cc
@@ -27,57 +27,100 @@ absl::Status RunGraph(std::string&& input_video_path) {
             output_stream: "VIDEO:input_stream"
             output_stream: "VIDEO_PRESTREAM:input_video_header"
         }
-        node {
-            calculator: "PacketResamplerCalculator"
-            input_stream: "DATA:input_stream"
-            output_stream: "DATA:throttled_input_stream"
-            
-            node_options: {
-                [type.googleapis.com/mediapipe.PacketResamplerCalculatorOptions] {
-                    frame_rate: 10
-                }
-            }
-        }
 
         # Detect pose
         node {
             calculator: "PoseDetectionCpu"
-            input_stream: "IMAGE:throttled_input_stream"
+            input_stream: "IMAGE:input_stream"
             output_stream: "DETECTIONS:pose_detections"
+        }
+        # in my understanding, this detection is originally a single detection
+        node {
+            calculator: "SplitDetectionVectorCalculator"
+            input_stream: "pose_detections"
+            output_stream: "pose_detection"
+            node_options: {
+                [type.googleapis.com/mediapipe.SplitVectorCalculatorOptions] {
+                    ranges: { begin: 0 end: 1 }
+                    element_only: true
+                }
+            }
+        }
+
+        # Image size
+        node {
+            calculator: "ImagePropertiesCalculator"
+            input_stream: "IMAGE_CPU:input_stream"
+            output_stream: "SIZE:input_image_size"
+        }
+
+        # Smoothing
+        node {
+            calculator: "DetectionToLandmarksCalculator"
+            input_stream: "DETECTION:pose_detection"
+            output_stream: "LANDMARKS:pose_landmarks"
+        }
+        node {
+            calculator: "LandmarksSmoothingCalculator"
+            input_stream: "NORM_LANDMARKS:pose_landmarks"
+            input_stream: "IMAGE_SIZE:input_image_size"
+            output_stream: "NORM_FILTERED_LANDMARKS:smooth_pose_landmarks"
+            node_options: {
+                [type.googleapis.com/mediapipe.LandmarksSmoothingCalculatorOptions] {
+                    velocity_filter: {
+                        window_size: 10
+                        velocity_scale: 0.001
+                        disable_value_scaling: true
+                    }
+                }
+            }
+        }
+        node {
+            calculator: "LandmarksToDetectionCalculator"
+            input_stream: "NORM_LANDMARKS:smooth_pose_landmarks"
+            output_stream: "DETECTION:smooth_pose_detection"
+        }
+
+        # Calculate rect
+        node {
+            calculator: "AlignmentPointsRectsCalculator"
+            input_stream: "DETECTION:smooth_pose_detection"
+            input_stream: "IMAGE_SIZE:input_image_size"
+            output_stream: "NORM_RECT:smooth_pose_rect"
+            node_options: {
+                [type.googleapis.com/mediapipe.DetectionsToRectsCalculatorOptions] {
+                    rotation_vector_start_keypoint_index: 0
+                    rotation_vector_end_keypoint_index: 1
+                    rotation_vector_target_angle_degrees: 90
+                }
+            }
         }
 
         # Render pose detections
         node {
-            calculator: "DetectionsToRenderDataCalculator"
-            input_stream: "DETECTIONS:pose_detections"
-            output_stream: "RENDER_DATA:pose_detections_render_data"
+            calculator: "RectToRenderDataCalculator"
+            input_stream: "NORM_RECT:smooth_pose_rect"
+            output_stream: "RENDER_DATA:smooth_pose_render_data"
             node_options: {
-                [type.googleapis.com/mediapipe.DetectionsToRenderDataCalculatorOptions] {
+                [type.googleapis.com/mediapipe.RectToRenderDataCalculatorOptions] {
                     thickness: 1.0
                     color { r: 255 g: 0 b: 0 }
-                    render_detection_id: true
                 }
             }
         }
         node {
             calculator: "AnnotationOverlayCalculator"
-            input_stream: "IMAGE:throttled_input_stream"
-            input_stream: "pose_detections_render_data"
+            input_stream: "IMAGE:input_stream"
+            input_stream: "smooth_pose_render_data"
             output_stream: "IMAGE:annotated_image_stream"
         }
 
         # Output video
         node {
-            calculator: "VideoPreStreamCalculator"
-            input_stream: "FRAME:annotated_image_stream"
-            input_stream: "VIDEO_PRESTREAM:input_video_header"
-            output_stream: "generated_video_header"
-        }
-        node {
             calculator: "OpenCvVideoEncoderCalculator"
             input_side_packet: "OUTPUT_FILE_PATH:output_video_path"
             input_stream: "VIDEO:annotated_image_stream"
-            input_stream: "VIDEO_PRESTREAM:generated_video_header"
+            input_stream: "VIDEO_PRESTREAM:input_video_header"
             node_options: {
                 [type.googleapis.com/mediapipe.OpenCvVideoEncoderCalculatorOptions]: {
                     codec: "avc1"
