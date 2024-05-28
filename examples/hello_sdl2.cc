8a1152d83e44ef6d4a7f28eda131a66ada10ce66
Hitoshi Amano <seijro@gmail.com>
2024-05-28 21:18:54 +0900

    simple video player

--- examples/hello_sdl2.cc
+++ examples/hello_sdl2.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <filesystem>
+#include <unordered_map>
+#include <thread>
+#include <chrono>
 
 #include "absl/status/status.h"
 #include "absl/flags/flag.h"
@@ -11,11 +14,20 @@
 
 #include <SDL2/SDL.h>
 
-ABSL_FLAG(std::string, png_path, "", "Path to a PNG file to show");
+ABSL_FLAG(std::string, video_path, "", "Path to a Video file to show");
+
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator() (const std::pair<T1, T2>& p) const {
+        auto h1 = std::hash<T1>{}(p.first);
+        auto h2 = std::hash<T2>{}(p.second);
+        return h1 ^ h2;
+    }
+};
 
 const int window_width = 800;
 
-absl::Status RunRenderer(SDL_Window* window, const std::filesystem::path& png_path) {
+absl::Status RunRenderer(SDL_Window* window, const std::filesystem::path& video_path) {
     std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer(
             SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC),
             SDL_DestroyRenderer
@@ -29,62 +41,98 @@ absl::Status RunRenderer(SDL_Window* window, const std::filesystem::path& png_pa
     SDL_GetRendererInfo(renderer.get(), &renderer_info);
     std::cout << "Renderer: " << renderer_info.name << std::endl;
 
-    cv::Mat image = cv::imread(png_path.string());
-    if (image.empty()) {
-        return absl::InternalError("Failed to load image");
-    }
+    std::unordered_map<std::pair<int, int>, std::shared_ptr<SDL_Texture>, pair_hash> texture_cache;
 
-    double aspect_ratio = static_cast<double>(image.cols) / static_cast<double>(image.rows);
-    SDL_SetWindowSize(window, window_width, static_cast<int>(static_cast<double>(window_width) / aspect_ratio));
+    cv::VideoCapture cap(video_path.string());
 
-    std::unique_ptr<SDL_Surface, decltype(&SDL_FreeSurface)> surface(
-            SDL_CreateRGBSurfaceFrom(
-                image.data,
-                image.cols,
-                image.rows,
-                image.elemSize() * 8,
-                image.step,
-                0xff0000, 0x00ff00, 0x0000ff, 0
-                ),
-            SDL_FreeSurface
-            );
-    if (surface == nullptr) {
-        std::string error(SDL_GetError());
-        return absl::InternalError(error);
-    }
+    // here is the 0 msec
+    std::chrono::milliseconds start_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
 
-    std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> texture(
-            SDL_CreateTextureFromSurface(renderer.get(), surface.get()),
-            SDL_DestroyTexture
-            );
+    int frame_index = 0;
+    while (cap.isOpened()) {
+        double frame_time = cap.get(cv::CAP_PROP_POS_MSEC);
+        std::chrono::milliseconds current_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+        std::chrono::milliseconds elapsed_time = current_time - start_time;
+        if (elapsed_time.count() < frame_time) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(frame_time - elapsed_time.count())));
+        }
 
-    SDL_RenderClear(renderer.get());
-    SDL_RenderCopy(renderer.get(), texture.get(), nullptr, nullptr);
-    SDL_RenderPresent(renderer.get());
+        cv::Mat frame;
+        cap >> frame;
+        if (frame.empty()) {
+            break;
+        }
 
-    SDL_Event e;
-    bool quit = false;
-    while (!quit){
+        int frame_width = frame.cols;
+        int frame_height = frame.rows;
+
+        double aspect_ratio = static_cast<double>(frame_width) / static_cast<double>(frame_height);
+        SDL_SetWindowSize(window, window_width, static_cast<int>(static_cast<double>(window_width) / aspect_ratio));
+
+        std::pair<int, int> frame_size(frame.cols, frame.rows);
+        auto it = texture_cache.find(frame_size);
+        if (it == texture_cache.end()) {
+            std::shared_ptr<SDL_Texture> texture(
+                    SDL_CreateTexture(
+                        renderer.get(),
+                        SDL_PIXELFORMAT_BGR24,
+                        SDL_TEXTUREACCESS_STREAMING,
+                        frame_size.first,
+                        frame_size.second
+                        ),
+                    SDL_DestroyTexture
+                    );
+            if (texture == nullptr) {
+                std::string error(SDL_GetError());
+                return absl::InternalError(error);
+            }
+            texture_cache[frame_size] = texture;
+        }
+        SDL_Texture* texture = texture_cache[frame_size].get();
+        ABSL_ASSERT(texture != nullptr);
+
+        std::unique_ptr<SDL_Surface, decltype(&SDL_FreeSurface)> surface(
+                SDL_CreateRGBSurfaceFrom(
+                    frame.data,
+                    frame.cols,
+                    frame.rows,
+                    frame.elemSize() * 8,
+                    frame.step,
+                    0xff0000, 0x00ff00, 0x0000ff, 0
+                    ),
+                SDL_FreeSurface
+                );
+        if (surface == nullptr) {
+            std::string error(SDL_GetError());
+            return absl::InternalError(error);
+        }
+        
+        SDL_UpdateTexture(texture, nullptr, surface->pixels, surface->pitch);
+
+        SDL_RenderClear(renderer.get());
+        SDL_RenderCopy(renderer.get(), texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer.get());
+
+        SDL_Event e;
         while (SDL_PollEvent(&e)){
             if (e.type == SDL_QUIT){
                 std::cout << "Quit event" << std::endl;
-                quit = true;
+                return absl::OkStatus();
             }
             if (e.type == SDL_KEYDOWN){
                 std::cout << "Key down event" << std::endl;
-                quit = true;
+                return absl::OkStatus();
             }
             if (e.type == SDL_MOUSEBUTTONDOWN){
                 std::cout << "Mouse button down event" << std::endl;
-                quit = true;
+                return absl::OkStatus();
             }
         }
     }
-
     return absl::OkStatus();
 }
 
-absl::Status RunWindow(const std::filesystem::path& png_path) {
+absl::Status RunWindow(const std::filesystem::path& video_path) {
     std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window(
             SDL_CreateWindow(
                 "Hello, SDL2!",
@@ -103,14 +151,14 @@ absl::Status RunWindow(const std::filesystem::path& png_path) {
 
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
 
-    return RunRenderer(window.get(), png_path);
+    return RunRenderer(window.get(), video_path);
 }
 
-absl::Status RunApp(const std::filesystem::path& png_path) {
+absl::Status RunApp(const std::filesystem::path& video_path) {
     std::cout << "Hello, SDL2!" << std::endl;
     SDL_Init(SDL_INIT_VIDEO);
 
-    absl::Status status = RunWindow(std::move(png_path));
+    absl::Status status = RunWindow(std::move(video_path));
 
     SDL_Quit();
     return status;
@@ -118,12 +166,12 @@ absl::Status RunApp(const std::filesystem::path& png_path) {
 
 int main(int argc, char* argv[]) {
     absl::ParseCommandLine(argc, argv);
-    std::string png_path = absl::GetFlag(FLAGS_png_path);
-    if (png_path.empty()) {
-        std::cerr << "Error: --png_path is required" << std::endl;
+    std::string video_path = absl::GetFlag(FLAGS_video_path);
+    if (video_path.empty()) {
+        std::cerr << "Error: --video_path is required" << std::endl;
         return 1;
     }
-    std::filesystem::path path(png_path);
+    std::filesystem::path path(video_path);
 
     absl::Status status = RunApp(std::move(path));
     if (!status.ok()) {
