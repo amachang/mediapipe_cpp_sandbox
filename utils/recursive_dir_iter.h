0981de7d84d4fe2f294f5812c43c48940b05fe33
Hitoshi Amano <seijro@gmail.com>
2024-06-13 19:11:44 +0900

    fix a bit

--- utils/recursive_dir_iter.h
+++ utils/recursive_dir_iter.h
@@ -6,47 +6,47 @@
 class RecursiveDirIterator {
     private:
         void advance() {
-            while (!dir_iter_stack.empty()) {
-                std::filesystem::directory_iterator& iter = dir_iter_stack.top();
+            while (!dir_iter_stack_.empty()) {
+                std::filesystem::directory_iterator& iter = dir_iter_stack_.top();
 
                 if (iter == std::filesystem::end(iter)) {
-                    dir_iter_stack.pop();
+                    dir_iter_stack_.pop();
                     continue;
                 } else {
                     const std::filesystem::path& path = *iter;
                     if (std::filesystem::is_directory(path)) {
-                        dir_iter_stack.push(std::filesystem::directory_iterator(path));
+                        dir_iter_stack_.push(std::filesystem::directory_iterator(path));
                         ++iter;
                     } else {
-                        current_file = path;
+                        current_file_ = path;
                         ++iter;
                         return;
                     }
                 }
             }
 
-            current_file = std::filesystem::path();
+            current_file_ = std::filesystem::path();
         }
 
-        std::stack<std::filesystem::directory_iterator> dir_iter_stack;
-        std::filesystem::path current_file;
+        std::stack<std::filesystem::directory_iterator> dir_iter_stack_;
+        std::filesystem::path current_file_;
 
     public:
         RecursiveDirIterator() = default;
 
-        RecursiveDirIterator(const std::filesystem::path& path) : dir_iter_stack() {
+        RecursiveDirIterator(const std::filesystem::path& path) : dir_iter_stack_() {
             if (!std::filesystem::exists(path)) {
                 throw std::invalid_argument("Path does not exist: " + path.string());
             }
             if (!std::filesystem::is_directory(path)) { 
                 throw std::invalid_argument("Path is not a directory: " + path.string());
             }
-            dir_iter_stack.push(std::filesystem::directory_iterator(path));
+            dir_iter_stack_.push(std::filesystem::directory_iterator(path));
             advance();
         }
 
         const std::filesystem::path& operator*() const {
-            return current_file;
+            return current_file_;
         }
 
         RecursiveDirIterator& operator++() {
@@ -55,7 +55,7 @@ class RecursiveDirIterator {
         }
 
         bool operator==(const RecursiveDirIterator& other) const {
-            return current_file == other.current_file && dir_iter_stack.empty() && other.dir_iter_stack.empty();
+            return current_file_ == other.current_file_ && dir_iter_stack_.empty() && other.dir_iter_stack_.empty();
         }
 
         bool operator!=(const RecursiveDirIterator& other) const {
@@ -65,10 +65,12 @@ class RecursiveDirIterator {
 
 class RecursiveDirIterable {
     public:
-        RecursiveDirIterable(const std::filesystem::path& path) : root_path(path) {}
+        RecursiveDirIterable() = default;
+
+        RecursiveDirIterable(const std::filesystem::path& root_path) : root_path_(root_path) {}
 
         RecursiveDirIterator begin() const {
-            return RecursiveDirIterator(root_path);
+            return RecursiveDirIterator(root_path_);
         }
 
         RecursiveDirIterator end() const {
@@ -76,6 +78,6 @@ class RecursiveDirIterable {
         }
 
     private:
-        std::filesystem::path root_path;
+        std::filesystem::path root_path_;
 };
 
